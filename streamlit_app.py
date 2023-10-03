590f69addb5f136ff06f91eeb17e3df794846546
Maxim <maxim.oweyssi@est.org.uk>
2023-10-03 17:59:58 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,14 +4,12 @@ from shapely.geometry import Point
 import requests
 import folium
 from folium.plugins import MarkerCluster
-from streamlit_folium import st_folium
 from pyproj import Transformer
+from streamlit_folium import st_folium
 
 # Load the SHP file into a GeoDataFrame
 shp_file_path = 'pub_sca.shp'
-
-gdf = gpd.read_file(shp_file_path)
-gdf.crs = 'EPSG:27700'  # Set the CRS to British National Grid (EPSG:27700)
+gdf = gpd.read_file(shp_file_path, crs='EPSG:27700')
 
 # Define a transformer to convert between CRS
 transformer = Transformer.from_crs('EPSG:4326', 'EPSG:27700', always_xy=True)
@@ -22,20 +20,19 @@ def get_coordinates_for_postcode(postcode):
     response = requests.get(api_url)
     if response.status_code == 200:
         data = response.json()
-        latitude = data["result"]["latitude"]
-        longitude = data["result"]["longitude"]
-        return latitude, longitude
-    else:
-        raise Exception(f"Failed to retrieve coordinates for postcode {postcode}")
+        return data["result"]["latitude"], data["result"]["longitude"]
+    raise Exception(f"Failed to retrieve coordinates for postcode {postcode}")
 
-# Define the UK postcode you want to check
+# Streamlit app title and input
 st.title("Smoke control area checker")
 postcode_to_check = st.text_input("Enter Scottish postcode to check")
+
+if not postcode_to_check:
+    st.stop()
 
 # Get the coordinates (latitude and longitude) for the given postcode
 try:
     latitude, longitude = get_coordinates_for_postcode(postcode_to_check)
-    #st.write(f"Latitude: {latitude}, Longitude: {longitude}")
 except Exception as e:
     st.error(e)
     st.stop()
@@ -45,6 +42,8 @@ point = Point(transformer.transform(longitude, latitude))
 
 # Check if the point is within any of the geometries in the GeoDataFrame
 is_within_area = gdf.geometry.contains(point).any()
+
+# Display the result
 if is_within_area:
     st.success(f"The postcode {postcode_to_check} is within a smoke control area.")
 else:
@@ -58,14 +57,12 @@ marker_cluster = MarkerCluster().add_to(m)
 folium.Marker([latitude, longitude], icon=folium.Icon(color='red')).add_to(marker_cluster)
 
 # Add all GeoDataFrame shapes as overlays to the map
- 
 for idx, row in gdf.iterrows():
     sim_geo = gpd.GeoSeries(row["geometry"]).simplify(tolerance=0.02)
     sim_geo.crs = 'EPSG:27700'
     goodcrs = sim_geo.to_crs('EPSG:4326')
-    geo_json = goodcrs.to_json()
+    geo_json = goodcrs.__geo_interface__
     folium.GeoJson(geo_json, name=f"Shape {idx}").add_to(m)
 
-# Display the folium map using st_folium
-st_folium(m,width="full")
-
+# Display the Folium map
+st_folium(m, width="100%", height=500)
