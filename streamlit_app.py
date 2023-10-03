64cca95a8d9afb5514a3a7e9e1b703584f0a838c
Maxim <maxim.oweyssi@est.org.uk>
2023-10-03 16:36:21 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,7 +60,7 @@ folium.Marker([latitude, longitude], icon=folium.Icon(color='red')).add_to(marke
 for idx, row in gdf.iterrows():
     sim_geo = gpd.GeoSeries(row["geometry"]).simplify(tolerance=0.001)
     geo_j = sim_geo.to_json()
-    folium.GeoJson(geo_j).add_to(m)
+    geo_j.add_to(m)
 
 # Display the folium map using st_folium
 st_folium(m)
